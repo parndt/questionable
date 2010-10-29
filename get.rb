ff981111a310f380a7fc67dc8fc90f1faa7f85d9
Philip Arndt <parndt@gmail.com>
2010-10-29 16:41:59 +1300

    Seems easier to use YAML.

--- get.rb
+++ get.rb
@@ -2,14 +2,17 @@
 require 'net/http'
 require 'rubygems'
 require 'hpricot'
+require 'fileutils'
+require 'pathname'
+require 'yaml'
 
 comics = []
-urls = []
-# add urls in the format:
-# urls << ["Title", "http://site.com"]
-urls.each do |title, url|
+unless (config_file = Pathname.new(File.expand_path('../config.yml', __FILE__))).exist?
+  FileUtils::cp "#{config_file}.example", config_file.to_s
+end
+(urls = YAML::load(config_file.read)['urls']).each do |c|
  begin
-    uri = ::URI.parse(url)
+    uri = ::URI.parse((url = c['url']))
     resp = ::Net::HTTP.get_response(uri)
     if resp.class.name == "Net::HTTPFound" and resp.inspect =~ /302/
       resp = ::Net::HTTP.get_response(URI.parse("#{url.gsub('/comics/', resp['location'])}"))
@@ -20,7 +23,7 @@ urls.each do |title, url|
     images << html.search("//img[@src*=db/files/Comics/]")
 
     images = images.sort_by { |i, j| i.to_s <=> j.to_s } if images.size > 1
-    comics << "<div id='#{title}'>"
+    comics << "<div id='#{c['title']}'>"
     comics << images.flatten.collect do |i|
       if i.to_s =~ /http:\/\//
         image = i.to_s
@@ -38,7 +41,7 @@ end
 dir = File.dirname(__FILE__)
 File.delete(File.join(dir, "latest.html")) if File.exists?(File.join(dir, "latest.html"))
 unless comics.empty?
-  titles = urls.collect{|t,u| "<a href='##{t}'>#{t}</a>"}
+  titles = urls.collect{|h| "<a href='##{h['title']}'>#{h['title']}</a>"}
   File.open(File.join(dir, "latest.html"), "w").puts(
     "<html><head><link rel='stylesheet' href='ui.css'/><script src='jquery-min.js'></script><script src='jquery-ui-custom-min.js'></script>
      <script>$(document).ready(function(){$('#tabs').tabs({tabTemplate: '<li><a href=\"\#{href}\">\#{label}</a></li>'})});</script>
