68aa5e3e6b558884a7192ebdafb65051add9a018
Tim Carey-Smith <tim@spork.in>
2013-05-09 23:17:04 +1200

    Create the Comics in futures

--- lib/questionable/fetcher.rb
+++ lib/questionable/fetcher.rb
@@ -1,32 +1,41 @@
 module Questionable
   class Fetcher
+    include Celluloid
+
     def initialize(config_filename, output_filename)
       @config_filename = config_filename
       @output_filename = output_filename
     end
 
     def run
-      comics = {}
       unless @config_filename.exist?
         FileUtils::cp "#{@config_filename}.example", @config_filename.to_s
       end
-      comics = YAML::load(@config_filename.read)['urls'].map do |h|
-        Comic.new(h['title'], h['url'])
-      end
-      futures = comics.map do |comic|
-        comic.future(:fetch)
-      end
-      futures.each(&:value)
       @output_filename.delete if @output_filename.exist?
-      unless comics.empty?
+      @comics = fetch_comics
+
+      unless @comics.empty?
         template = File.read(File.expand_path("../../../views/index.html.haml", __FILE__))
         engine = Haml::Engine.new(template, format: :html5)
 
-        @output_filename.open("w").puts engine.render(nil, comics: comics)
+        @output_filename.open("w").puts engine.render(nil, comics: @comics)
 
         `open #{@output_filename}`
       else
         $stdout.puts "Nothing found, sorry."
+      end
+    end
+
+    def fetch_comics
+      futures = YAML::load(@config_filename.read)['urls'].map do |h|
+        future(:build_comic, h['title'], h['url'])
+      end
+      futures.map(&:value)
+    end
+
+    def build_comic(title, url)
+      Comic.new(title, url).tap do |comic|
+        comic.fetch
       end
     end
   end
