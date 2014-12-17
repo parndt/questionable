1a13af9ceb62e4d4e9b04e6e3de485ae516ba24a
Philip Arndt <git@p.arndt.io>
2014-12-18 10:54:14 +1300

    Support '//' image src urls

--- lib/questionable/comic.rb
+++ lib/questionable/comic.rb
@@ -20,18 +20,23 @@ module Questionable
     def parse(html)
       images = html.search("//img[@src*=comics/]")
       images << html.search("//img[@src*=#{Time.now.year}/#{@url.split('//')[1].split('.').first}]")
-      images << html.search("//img[@src*=db/files/Comics/]")
+      images << html.search("//img[@src*=/comics/]")
       images << html.search('#comic > img')
 
-      images.flatten.uniq.map(&:to_s).reject{|i| i =~ /(facebook|twitter).gif/}.map do |image|
-        if image !~ /http:\/\//
-          image = image.gsub(/src=[\"|\']/){|m| "#{m}#{@url}/"}.
+      images.flatten.uniq.map do |image|
+        next if image[:src] =~ /(facebook|twitter).gif/
+
+        if image[:src] !~ %r{\A(http:)?//}
+          image[:src] = "#{@url}#{image[:src]}".gsub('///', '//').
                         gsub("#{@url}#{@url}", @url).
                         gsub("#{@url}//", "#{@url}/")
         end
 
+        # ensure // links are http:// instead.
+        image[:src] = image[:src].gsub(%r{\A//}, 'http://')
+
         image
-      end
+      end.compact
     end
 
     def page
